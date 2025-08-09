37817d996d427362f6ab57db052f81cd45a9ec3f
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2025-08-09 12:57:42 +0100

    footer + hero banner button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,136 @@
+"use client"
+
 import TopNavBar from "@/components/NavBar";
 import Button from "@/components/Button";
 import {cn} from "@/Utilities";
 import Footer from "@/components/Footer";
+import {motion, } from "framer-motion";
 
 export default function Home() {
+
     return (
         <div className="xl:max-w-[1920px] xl:mx-auto">
             <TopNavBar/>
-            <Hero/>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: .5
+                    }
+                }}
+                viewport={{once: true}}
+            >
+                <Hero/>
+            </motion.div>
+
             <Featured/>
-            <Feature1/>
-            <Feature2/>
-            <Feature3/>
-            <Container/>
-            <About1/>
-            <CTA2/>
-            <Integrations/>
-            <CTA/>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <Feature1/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <Feature2/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <Feature3/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <Container/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <About1/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <CTA2/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <Integrations/>
+            </motion.div>
+
+            <motion.div
+                initial={{opacity: 0}}
+                whileInView={{
+                    opacity: 1,
+                    transition: {
+                        duration: 1
+                    }
+                }}
+                viewport={{once: true, margin: "0px 0px -200px 0px",}}
+            >
+                <CTA/>
+            </motion.div>
+
             <Footer/>
         </div>
     );
@@ -27,6 +141,32 @@ function Hero() {
     return (
         <div className="pt-10 px-5 xl:pl-16 grid md:grid-cols-2 max-md:gap-4">
             <div className="flex flex-col justify-center gap-7">
+                <a href="https://ui8.net/wily/products/social-schedule-website-template" target="_blank" className="hero-banner-button relative rounded-[.875rem] flex text-sm text-neutral-500">
+                    <svg className="shrink-0" xmlns="http://www.w3.org/2000/svg" height="20" width="20"
+                         viewBox="0 0 24 24" aria-hidden="true">
+                        <use xlinkHref="#sparkle"></use>
+                        <defs>
+                            <symbol id="sparkle" stroke="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M10.359 3.533 C 10.347 3.579,10.328 3.729,10.317 3.867 C 10.231 4.927,9.870 6.002,9.308 6.867 C 8.254 8.489,6.294 9.627,4.050 9.919 C 3.830 9.947,3.590 9.983,3.517 9.998 L 3.383 10.025 3.562 10.029 C 3.809 10.035,4.375 10.120,4.833 10.221 C 5.428 10.352,6.067 10.579,6.650 10.865 C 7.440 11.252,7.890 11.560,8.429 12.081 C 9.566 13.183,10.161 14.485,10.331 16.247 C 10.370 16.646,10.400 16.743,10.400 16.468 C 10.400 16.020,10.630 14.922,10.849 14.319 C 11.096 13.642,11.417 13.020,11.796 12.485 C 12.064 12.105,12.659 11.502,13.067 11.195 C 13.456 10.902,14.234 10.497,14.683 10.353 C 15.214 10.184,15.999 10.035,16.375 10.034 C 16.647 10.033,16.548 9.983,16.190 9.940 C 15.261 9.830,14.172 9.448,13.373 8.953 C 11.565 7.831,10.538 5.988,10.391 3.600 C 10.383 3.480,10.377 3.467,10.359 3.533 "
+                                    fill="currentColor" stroke="none" fillRule="evenodd"></path>
+                            </symbol>
+
+                            <symbol id="arrow-right" stroke="currentColor" viewBox="0 0 20 20"><path d="M9.750 4.963 C 9.591 5.036,9.520 5.110,9.451 5.275 C 9.386 5.431,9.386 5.569,9.451 5.726 C 9.487 5.812,10.049 6.397,11.276 7.625 L 13.050 9.400 9.215 9.400 L 5.381 9.400 5.232 9.475 C 4.795 9.696,4.795 10.304,5.232 10.525 L 5.381 10.600 9.215 10.600 L 13.050 10.600 11.276 12.375 C 10.049 13.603,9.487 14.188,9.451 14.274 C 9.386 14.431,9.386 14.569,9.451 14.725 C 9.520 14.890,9.591 14.964,9.750 15.037 C 9.920 15.115,10.082 15.115,10.250 15.036 C 10.416 14.958,15.013 10.363,15.065 10.223 C 15.110 10.103,15.110 9.897,15.065 9.777 C 15.013 9.637,10.416 5.042,10.250 4.964 C 10.082 4.885,9.920 4.885,9.750 4.963 " stroke="none" fill-rule="evenodd" fill="currentColor"></path></symbol>
+                        </defs>
+                    </svg>
+
+                    {/*<span className="text-caption shrink-0 font-medium">New</span>*/}
+
+                    {/*<div className="h-4/5 w-[1px] mx-0.5 bg-[#00000011]"></div>*/}
+
+                    <span className="text-caption web-u-trim-1">Check out the design file</span>
+
+                    <svg className="shrink-0" xmlns="http://www.w3.org/2000/svg" height="20" width="20"
+                         viewBox="0 0 24 24" aria-hidden="true">
+                        <use xlinkHref="#arrow-right"></use>
+                    </svg>
+                </a>
                 <div className="grid grid-cols-2 gap-4 lg:flex">
                     <img src="/img/hero/p-of-week.svg" alt=""/>
                     <img src="/img/hero/b-of-product.svg" alt=""/>
@@ -325,15 +465,17 @@ function Integrations() {
 
 function CTA() {
     return (
-        <div className="py-10 px-4 md:px-10 min-h-96 bg-[url('/img/cta-bg.jpg')] bg-[-50%] md:bg-[50%] text-white md:flex">
+        <div
+            className="py-10 px-4 md:px-10 min-h-96 bg-[url('/img/cta-bg.jpg')] bg-[-50%] md:bg-[50%] text-white md:flex">
             <div className="space-y-5 md:flex md:flex-col md:justify-center">
                 <p className="text-3xl font-semibold leading-9">Scalable Enterprise Social
                     Solutions</p>
-                <p className="text-disabled-300 text-sm">Need a comprehensive platform your team will love to use? Discover how
+                <p className="text-disabled-300 text-sm">Need a comprehensive platform your team will love to use?
+                    Discover how
                     you can make an impact across your entire business, from industry insights and competitive
                     intelligence to customer service, data, and analytics.</p>
 
-                <Button text="Request a demo" style="fill" className="border-none w-fit" />
+                <Button text="Request a demo" style="fill" className="border-none w-fit"/>
             </div>
 
             <img src="/img/cta-img.png" alt="" className="max-md:mt-10 md:w-1/2"/>
